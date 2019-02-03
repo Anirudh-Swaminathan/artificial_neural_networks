0e56a91eab17cbdc06ff08934cf0c95822c93e0e
Anirudh Swaminathan <aniswami97@gmail.com>
2019-02-03 11:50:16 +0530

    Modified .gitignore. PEP8 standards maintain
    Modified .gitignore to ignore the .idea folder generated by PyCharm.
    Reformatted the script to conform to PEP8 standards

--- hw1/and_forward.py
+++ hw1/and_forward.py
@@ -2,8 +2,10 @@
 
 """Script to build a feedforward AND gate"""
 
+
 class Neuron(object):
     """A Neuron object. It contains inputs, weights and an activation function"""
+
     def __init__(self, inp_lis, w_lis):
         """Constructor"""
         self.y = 0
@@ -13,30 +15,30 @@ class Neuron(object):
 
     def binary_func(self):
         """Returns 1 if the weighted sum of inputs is >0, else -1"""
-        return -1 if self.y<0 else 1
-    
+        return -1 if self.y < 0 else 1
+
     def activation_function(self):
         """Computes the output of the neuron"""
         self.out = self.binary_func()
 
-
     def calc_net(self):
         """Calculate the weighted sum of inputs"""
         net = 0
-        out_lis = [a*b for a,b in zip(self.inp_list, self.weight_list)]
+        out_lis = [a * b for a, b in zip(self.inp_list, self.weight_list)]
         self.y = sum(out_lis)
 
     def forward_prop(self):
         """Does one forward prop through the neuron"""
         self.calc_net()
         self.activation_function()
-        
+
     def display_stuff(self):
         """Displays the neurons, the weights, the net and the output"""
         print("i1 | i2 | b  | Net | Output")
         print(*self.inp_list, self.y, self.out, sep="  | ")
         print("Weights used are: ")
         print(*self.weight_list)
+
 
 def main():
     """The main method"""
@@ -51,11 +53,11 @@ def main():
     n1 = Neuron(is1, ws)
     n1.forward_prop()
     n1.display_stuff()
-    
+
     n2 = Neuron(is2, ws)
     n2.forward_prop()
     n2.display_stuff()
-    
+
     n3 = Neuron(is3, ws)
     n3.forward_prop()
     n3.display_stuff()
@@ -64,6 +66,6 @@ def main():
     n4.forward_prop()
     n4.display_stuff()
 
+
 if __name__ == "__main__":
     main()
-
